c5a30eaeaf284bdf9fa5a1d9066f15d1663656b7
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-22 21:06:29 +0900

    Update eval.py

--- eval/eval.py
+++ eval/eval.py
@@ -131,6 +131,26 @@ def setup_custom_parser():
         action="store_true",
         help="Run evalutaions in debug mode on a few examples",
     )
+
+    # FIXME
+    parser.add_argument(
+        "--non_thinking",
+        action="store_true",
+        default=False,
+    )
+
+    parser.add_argument(
+        "--thinking_budget",
+        type=int,
+        default=None,
+    )
+
+    parser.add_argument(
+        "--thinking_token",
+        type=str,
+        default=None,
+    )
+
     return parser
 
 
@@ -375,6 +395,10 @@ def cli_evaluate(args: Optional[argparse.Namespace] = None) -> None:
         seed=args.seed,
         task_list=task_list,
         system_instruction=args.system_instruction,
+        # FIXME
+        non_thinking=args.non_thinking,
+        thinking_budget=args.thinking_budget,
+        thinking_token=args.thinking_token,
     )
     pretrain_task_manager = PretrainTaskManager(args.verbosity, include_path=args.include_path)
 
