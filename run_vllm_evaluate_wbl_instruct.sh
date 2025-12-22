b7242b416e72cfa5068c9b552658ca0ddaba012f
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-22 23:16:45 +0900

    Update run_vllm_evaluate_wbl_instruct.sh

--- run_vllm_evaluate_wbl_instruct.sh
+++ run_vllm_evaluate_wbl_instruct.sh
@@ -65,6 +65,6 @@ python -m eval.eval \
     --model_args pretrained=${MODEL_PATH},pipeline_parallel_size=8,trust_remote_code=True \
     --tasks AALCR \
     --batch_size auto \
-    --max_tokens 262144 \
+    --max_tokens 32768 \
     --apply_chat_template \
     --output_path results \
