2ad47c9b751af344031afec93dad8ec3afec844d
Jeongho Ju <jujeongho@ajou.ac.kr>
2025-12-22 21:08:01 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/GPQADiamond/eval_instruct.py
+++ eval/chat_benchmarks/GPQADiamond/eval_instruct.py
@@ -38,6 +38,10 @@ class GPQADiamondBenchmark(BaseBenchmark):
         max_tokens: int = 32768,
         logger: Optional[logging.Logger] = None,
         system_instruction: Optional[str] = None,
+        # FIXME
+        non_thinking: Optional[bool] = False,
+        thinking_budget: Optional[int] = None,
+        thinking_token: Optional[str] = None,
     ):
         """
         Initialize GPQADiamond benchmark.
@@ -53,6 +57,10 @@ class GPQADiamondBenchmark(BaseBenchmark):
         self.seed = seed
         self.max_new_tokens = max_tokens
         self.n_repeat = 1 # FIXME
+        # FIXME
+        self.non_thinking = non_thinking
+        self.thinking_budget = thinking_budget
+        self.thinking_token = thinking_token
 
     def generate_responses(self, model: LM) -> Dict[str, Any]:
         """
@@ -95,8 +103,9 @@ class GPQADiamondBenchmark(BaseBenchmark):
 
                 templated_messages = self._prepare_messages(messages, model)
 
-                # FIXME: Non-thinking
-                # templated_messages = templated_messages + "<think>\n\n</think>\n\n"
+                # FIXME: Non-thinking Mode
+                if self.non_thinking:
+                    templated_messages = templated_messages + "<think>\n\n</think>\n\n"
 
                 instance = Instance(
                     "generate_until",
@@ -117,7 +126,7 @@ class GPQADiamondBenchmark(BaseBenchmark):
 
             # Generate model responses
             self.logger.info("Generating responses for GPQADiamond...")
-            outputs = self.compute(model, all_instances)
+            outputs = self.compute(model=model, inputs=all_instances, thinking_budget=self.thinking_budget, thinking_token=self.thinking_token) # FIXME
             all_outputs.append(outputs)
 
         # Return None early for non-primary ranks
