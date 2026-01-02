e3c9a506de931fe0f9a0dd15e1c3a52f523c7c7f
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-03 03:49:21 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/CLIcK/eval_instruct.py
+++ eval/chat_benchmarks/CLIcK/eval_instruct.py
@@ -46,7 +46,7 @@ class CLIcKBenchmark(BaseBenchmark):
         system_instruction: Optional[str] = None,
         # FIXME
         thinking_budget: Optional[int] = None,
-        parse_think: Optional[bool] = False,
+        parse_think: Optional[str] = None,
     ):
         super().__init__(logger=logger, system_instruction=system_instruction)
         self.dataset_name = "EunsuKim/CLIcK"
