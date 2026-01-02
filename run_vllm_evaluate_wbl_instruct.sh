968266b201f08de180f9b87372492fe91c521657
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-02 14:51:12 +0900

    Update run_vllm_evaluate_wbl_instruct.sh

--- run_vllm_evaluate_wbl_instruct.sh
+++ run_vllm_evaluate_wbl_instruct.sh
@@ -35,7 +35,7 @@ python -m eval.eval \
 #     --max_tokens 32768 \
 #     --apply_chat_template \
 #     --output_path results \
-#     --thinking_budget 16384 \
+#     --thinking_budget 24576 \
 #     --parse_think \
 
 python -m eval.eval \
