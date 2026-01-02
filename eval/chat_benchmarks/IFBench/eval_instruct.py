e272115c72acd8f9125ae2145891a69d2c72f47f
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-03 03:50:28 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/IFBench/eval_instruct.py
+++ eval/chat_benchmarks/IFBench/eval_instruct.py
@@ -23,7 +23,7 @@ class IFBenchBenchmark(BaseBenchmark):
         system_instruction: Optional[str] = None,
         # FIXME
         thinking_budget: Optional[int] = None,
-        parse_think: Optional[bool] = False,
+        parse_think: Optional[str] = None,
     ):
         """
         Initialize Instruction Following Benchmark
