d193dcf47f0bbbab8cb2e082296087fd0698c4db
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-03 03:48:35 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/AIME25/eval_instruct.py
+++ eval/chat_benchmarks/AIME25/eval_instruct.py
@@ -35,7 +35,7 @@ class AIME25Benchmark(BaseBenchmark):
         system_instruction: Optional[str] = None,
         # FIXME
         thinking_budget: Optional[int] = None,
-        parse_think: Optional[str] = False,
+        parse_think: Optional[str] = None,
     ):
         """
         Initialize AIME25 benchmark.
