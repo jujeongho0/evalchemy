778fd620438194845c2303272b272b22a610acce
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-05 19:15:33 +0900

    Update run_judge_results.py

--- eval/chat_benchmarks/MATH500/run_judge_results.py
+++ eval/chat_benchmarks/MATH500/run_judge_results.py
@@ -8,7 +8,7 @@ from pydantic import BaseModel
 from tqdm.asyncio import tqdm_asyncio
 
 client = AsyncOpenAI(
-    api_key=os.getenv("OPENAI_API_KEY"),, # TODO: You need to register your OpenAI API KEY.
+    api_key=os.getenv("OPENAI_API_KEY"), # TODO: You need to register your OpenAI API KEY.
     timeout=300.0,
     max_retries=1
 )
