45e2057f9d7d9c8d31cd23455549b631faf29b5c
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-06 12:09:47 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/MATH500/eval_instruct.py
+++ eval/chat_benchmarks/MATH500/eval_instruct.py
@@ -11,12 +11,9 @@ from run_judge_results import judge_all_responses
 
 from eval.task import BaseBenchmark
 
-# FIXME: Adopted from https://artificialanalysis.ai/methodology/intelligence-benchmarking#mathematical-questions
-PROMPT = """Solve the following math problem step by step. Put your answer inside \\boxed{{}}.
-
-{problem}
-
-Remember to put your answer inside \\boxed{{}}."""
+# Modified version of hendrycks_math with additional instruction to mark the solution with \\boxed
+# https://github.com/mlfoundations/evalchemy/blob/e70a45e41cb2ada273d6bb98e75dba303ec31f8b/eval/chat_benchmarks/AMC23/eval_instruct.py#L15
+PROMPT = """Problem: {problem}\nMark your solution with \\boxed\nAnswer:"""
 
 
 class MATH500Benchmark(BaseBenchmark):
