17dcbc5c78cfa53d3b3be33aeee270847ac51ab0
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-12 23:21:29 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/KMMLUPro/eval_instruct.py
+++ eval/chat_benchmarks/KMMLUPro/eval_instruct.py
@@ -54,7 +54,7 @@ class KMMLUProBenchmark(BaseBenchmark):
         system_instruction: Optional[str] = None,
         # FIXME
         thinking_budget: Optional[int] = None,
-        parse_think: Optional[str] = None,
+        parse_think: Optional[bool] = False,
     ):
         super().__init__(logger=logger, system_instruction=system_instruction)
         self.dataset_name = "LGAI-EXAONE/KMMLU-Pro"
