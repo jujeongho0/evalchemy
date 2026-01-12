c9b4cdd7a759ebd13a93c06af0a7dc74127fd4fc
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-12 23:22:03 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/LiveCodeBenchv6_official/eval_instruct.py
+++ eval/chat_benchmarks/LiveCodeBenchv6_official/eval_instruct.py
@@ -58,7 +58,7 @@ class LiveCodeBenchV6OfficialBenchmark(BaseBenchmark):
         system_instruction: Optional[str] = None,
         # FIXME
         thinking_budget: Optional[int] = None,
-        parse_think: Optional[str] = None,
+        parse_think: Optional[bool] = False,
     ):
         """
         Initialize LiveCodeBenchV6 benchmark.
