e19c12edcfa73685a73bccc1c2e6480397a5bfce
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-12 23:22:16 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/MATH500/eval_instruct.py
+++ eval/chat_benchmarks/MATH500/eval_instruct.py
@@ -32,7 +32,7 @@ class MATH500Benchmark(BaseBenchmark):
         system_instruction: Optional[str] = None,
         # FIXME
         thinking_budget: Optional[int] = None,
-        parse_think: Optional[str] = None,
+        parse_think: Optional[bool] = False,
     ):
         """
         Initialize MATH500 benchmark.
