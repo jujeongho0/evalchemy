c48d50dd4cb5a5052abbb5eadcb04792f8ab8b3e
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-12 23:20:31 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/HLE/eval_instruct.py
+++ eval/chat_benchmarks/HLE/eval_instruct.py
@@ -62,7 +62,7 @@ class HLESubsetBenchmark(BaseBenchmark):
         system_instruction: Optional[str] = None,
         # FIXME
         thinking_budget: Optional[int] = None,
-        parse_think: Optional[str] = None,
+        parse_think: Optional[bool] = False,
     ):
         """
         Initialize HLE benchmark.
