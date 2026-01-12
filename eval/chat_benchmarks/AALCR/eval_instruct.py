81e4638cfcc8fb2d2b7e0373936e8106ff5d7fc8
Jeongho Ju <jujeongho@ajou.ac.kr>
2026-01-12 23:18:29 +0900

    Update eval_instruct.py

--- eval/chat_benchmarks/AALCR/eval_instruct.py
+++ eval/chat_benchmarks/AALCR/eval_instruct.py
@@ -29,7 +29,7 @@ class AALCRBenchmark(BaseBenchmark):
         system_instruction: Optional[str] = None,
         # FIXME
         thinking_budget: Optional[int] = None,
-        parse_think: Optional[str] = None,
+        parse_think: Optional[bool] = False,
     ):
         super().__init__(logger=logger, system_instruction=system_instruction)
         self.debug = debug
